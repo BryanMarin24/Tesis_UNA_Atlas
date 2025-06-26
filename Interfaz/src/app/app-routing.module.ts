87f3d232070e015c91194d090e10360d076cb3e7
YeuAst <yeudany.astorga@gmail.com>
2025-06-25 19:58:08 -0600

    boostrap
    se agrega boostrap al proyecto

--- Interfaz/src/app/app-routing.module.ts
+++ Interfaz/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
 import { EstudiosComponent } from './estudios/estudios.component';
+
 import { ReporteriaComponent } from './reporteria/reporteria.component';
 import { AcercaComponent } from './acerca/acerca.component';
 import { PersonaComponent } from './persona/persona.component';
@@ -12,6 +13,7 @@ const routes: Routes = [
   { path: 'mantenimiento', component: PersonaComponent },
   { path: 'reporteria', component: ReporteriaComponent },
   { path: 'acerca', component: AcercaComponent },
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' },
   { path: '**', redirectTo: 'inicio' }
 ];
 
--- Interfaz/src/app/menu/menu.component.ts
+++ Interfaz/src/app/menu/menu.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [RouterModule], // ✅ Aquí se habilita routerLink
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.css'
+  styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
 
